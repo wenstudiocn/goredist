b2990b1cbd083211484f6f2ad22876b96464b1d7
yokyo <yyk882002@gmail.com>
2020-02-15 01:03:57 +0800

    update redis

--- redis.go
+++ redis.go
@@ -1,7 +1,6 @@
 package dist
 
 import (
-	"encoding/json"
 	"errors"
 	"time"
 
@@ -9,43 +8,31 @@ import (
 )
 
 var (
-	ErrNoRedisConf = errors.New("no redis addrs config in zookeeper")
+	ErrInvalidParams = errors.New("invalid parameters")
 )
-
-// 通用配置
-type ZkRedisConf struct {
-	Addrs    []string `json:"addrs"`
-	Password string   `json:"password"`
-	Db       int      `json:"db"`
-}
 
 // @data: redis 连接参数 json，来自 zookeeper
 // @conf
-func ConnectRedis(data []byte) (*redis.Client, error) {
-	conf := &ZkRedisConf{}
-	err := json.Unmarshal(data, conf)
-	if nil != err {
-		return nil, err
-	}
+func ConnectRedis(addrs []string, password string, db int) (*redis.Client, error) {
 	// 连接 redis
 	//TODO update to cluster version
-	rds_addr_len := len(conf.Addrs)
+	rds_addr_len := len(addrs)
 	if rds_addr_len <= 0 {
-		return nil, ErrNoRedisConf
+		return nil, ErrInvalidParams
 	}
-	rds_addr := conf.Addrs[0]
+	rds_addr := addrs[0]
 	//redis.NewClusterClient(redis.ClusterOptions{})
 	conn := redis.NewClient(&redis.Options{
 		Addr:         rds_addr,
-		Password:     conf.Password,
-		DB:           conf.Db,
+		Password:     password,
+		DB:           db,
 		DialTimeout:  3 * time.Second,
 		ReadTimeout:  3 * time.Second,
 		WriteTimeout: 5 * time.Second,
 		PoolSize:     10,
 		PoolTimeout:  60 * time.Second,
 	})
-	_, err = conn.Ping().Result()
+	_, err := conn.Ping().Result()
 	if nil != err {
 		return nil, err
 	}
