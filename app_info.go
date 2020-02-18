90592b1aabdd42554d8a5ea5ebde97ce9558a27b
yokyo <yyk882002@gmail.com>
2020-02-19 02:39:21 +0800

    enhance AppInfo

--- app_info.go
+++ app_info.go
@@ -2,6 +2,8 @@ package dist
 
 import (
 	"github.com/go-redis/redis"
+	"os"
+	"path"
 	"strconv"
 	"time"
 )
@@ -22,6 +24,10 @@ type AppInfo struct {
 	Zk *ZkClient				// zookeeper
 
 	enableLogger bool
+	logLevel int32
+	logPath string
+	logStdout bool
+
 	enableRdb bool
 	enableEv bool
 	enableMq bool
@@ -30,9 +36,12 @@ type AppInfo struct {
 
 type AppInfoOptions func(info *AppInfo)
 
-func AppEnableLogger(enable bool) AppInfoOptions {
+func AppEnableLogger(enable bool, logLevel int32, logPath string, logStdout bool) AppInfoOptions {
 	return func(ai *AppInfo) {
 		ai.enableLogger = enable
+		ai.logLevel = logLevel
+		ai.logPath = logPath
+		ai.logStdout = logStdout
 	}
 }
 
@@ -45,4 +54,26 @@ func NewAppInfo(programeName string, major, minor, revision int, id uint64, conf
 	ai.ConfFile = confFile
 
 	return ai
+}
+
+func (self *AppInfo)EnableLog(console bool, level int32, logPath string) error {
+	// level
+	lv, ok := LogLevelMap[int(level)]
+	if !ok {
+		return ErrInvalidParams
+	}
+
+	// path
+	f := self.InstName + ".log"
+	if len(logPath) > 0{
+		if _, err := os.Stat(logPath); err != nil {
+			return ErrInvalidParams
+		}
+		f = path.Join(logPath, f)
+	}
+	// logger
+	self.Log = NewSLogger(console, f, lv)
+	SetDefaultLogger(self.Log)
+
+	return nil
 }
